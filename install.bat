873b777670951df172f021f343972dec1ead1512
Adam Lněnička <70570107+AdamLnenicka@users.noreply.github.com>
2024-08-07 21:57:11 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -7,19 +7,15 @@ call venv\Scripts\activate
 REM Instalace závislostí
 pip install -r requirements.txt
 
-REM Vytvoření spustitelného souboru
-python -m PyInstaller --onefile --icon=icon.ico main.py
-
 REM Získání cesty k aktuálnímu skriptu
 set SCRIPT_PATH=%~dp0
-set DIST_PATH=%SCRIPT_PATH%dist
 set DESKTOP_PATH=%USERPROFILE%\Desktop
-set EXE_PATH=%DIST_PATH%\main.exe
-set ICON_PATH=%SCRIPT_PATH%icon.ico
+set PYTHON_PATH=%~dp0venv\Scripts\python.exe
+set SCRIPT_FILE=%SCRIPT_PATH%ambulance.py
 
 REM Vytvoření zástupce pomocí VBScriptu
 echo Vytváření zástupce na ploše...
-cscript create_shortcut.vbs "%EXE_PATH%" "%ICON_PATH%"
+cscript create_shortcut.vbs "%PYTHON_PATH%" "%SCRIPT_FILE%"
 
 if exist "%DESKTOP_PATH%\Ambulance App.lnk" (
     echo Zástupce byl úspěšně vytvořen na ploše.
