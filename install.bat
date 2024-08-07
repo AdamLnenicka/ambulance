777c1f9538ba422f6ff7328d18c3f746959ad247
Adam Lněnička <70570107+AdamLnenicka@users.noreply.github.com>
2024-08-07 22:02:15 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -10,12 +10,13 @@ pip install -r requirements.txt
 REM Získání cesty k aktuálnímu skriptu
 set SCRIPT_PATH=%~dp0
 set DESKTOP_PATH=%USERPROFILE%\Desktop
-set PYTHON_PATH=%~dp0venv\Scripts\python.exe
+set PYTHON_PATH=%SCRIPT_PATH%venv\Scripts\python.exe
 set SCRIPT_FILE=%SCRIPT_PATH%ambulance.py
+set ICON_FILE=%SCRIPT_PATH%icon.ico
 
 REM Vytvoření zástupce pomocí VBScriptu
 echo Vytváření zástupce na ploše...
-cscript create_shortcut.vbs "%PYTHON_PATH%" "%SCRIPT_FILE%"
+cscript create_shortcut.vbs "%PYTHON_PATH%" "%SCRIPT_FILE%" "%ICON_FILE%"
 
 if exist "%DESKTOP_PATH%\Ambulance App.lnk" (
     echo Zástupce byl úspěšně vytvořen na ploše.
